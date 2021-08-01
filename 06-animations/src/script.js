2bf59f24be285e3e2345e0b552d5dddec97e93f0
Hari Jeon <ori080525@gmail.com>
2021-08-01 23:09:32 +0900

    learn clock and gsap

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import * as THREE from "three";
+import gsap from "gsap";
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -31,18 +32,21 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
-// Time
-let time = Date.now();
+// Clock
+// const clock = new THREE.Clock();
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 });
 
 // Animation
 const tick = () => {
-  // Time
-  const currentTime = Date.now();
-  const deltaTime = currentTime - time;
-  time = currentTime;
+  // Clock
+  //   const elapsedTime = clock.getElapsedTime();
 
-  // Update objects
-  mesh.rotation.y += 0.001 * deltaTime; // regardless
+  //   // Update objects
+  //   camera.position.y = Math.sin(elapsedTime);
+  //   camera.position.x = Math.cos(elapsedTime);
+  //   camera.lookAt(mesh.position);
 
   // Render
   renderer.render(scene, camera);
